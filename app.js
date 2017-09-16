1831c981af4b712c89417a4e1e3e98f4482930a7
Justin Pu <justinpu@rocketmail.com>
2017-09-16 12:02:23 -0400

    Fix string length compilation error

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ function processMessage(messageText, senderID, userProfile, messageAttachments) 
       // Got a response from api.ai. Let's POST to Facebook Messenger
       let aiText = response.result.fulfillment.speech;
       console.log('Received response from api.ai: \'' + aiText + '\'');
-      if(aiText.size() > 0 && aiText.charAt(0) === '~') sendTextMessage(senderID, 'Subscribing to \'' + aiText.substr(1) + '\'');
+      if(aiText.length > 0 && aiText.charAt(0) === '~') sendTextMessage(senderID, 'Subscribing to \'' + aiText.substr(1) + '\'');
       else sendTextMessage(senderID, aiText);
     });
 
